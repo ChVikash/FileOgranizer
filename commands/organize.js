b5bc61336b84053d6402a9539c41ca43ab3e545b
= <vikashch1998@gmail.com>
2021-07-25 15:22:20 +0530

    oraganizer for file finish

--- commands/organize.js
+++ commands/organize.js
@@ -1,5 +1,53 @@
+let path = require("path");
+let fs =  require("fs");
+
+
+let types = {
+    media: ["mp4", "mkv"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"]
+}
+let arr = ["media" , "archives" , "documents" , "app" , "others"];
+
+
+let dirpath = path.join(process.cwd() ,"RandomFolder");
+
 function o(input){
-    console.log("organize commands executed with " + input);
+    for(let i = 0 ; i < arr.length ; ++i){
+        let p = path.join(dirpath , arr[i]);
+        if(fs.existsSync(p) == false){
+            fs.mkdirSync(p);
+        }
+    }
+    files = fs.readdirSync(input);
+    for(let i = 0 ; i < files.length ; ++i){
+        let fileordirpath = path.join(input , files[i] ) ;
+        let stats = fs.lstatSync(fileordirpath); 
+        if(stats.isFile() == true){
+            let flag = 0 ;
+            let ext = path.extname(fileordirpath);
+            //console.log(ext);
+            for(let i = 0 ; i < arr.length -1 ; ++i){
+                let key = arr[i];
+                //console.log(types[key]);
+                if(types[key].includes(ext.slice(1))){
+                    flag = 1 ;
+                    let filename = path.basename(fileordirpath);
+                    let destpath = path.join(dirpath , key ,filename);
+                    console.log("copying " , filename ," to " ,key);
+                    fs.copyFileSync(fileordirpath , destpath);
+                    console.log("copied" , filename);
+                    break ;
+                }
+            }
+            if(flag == 0){
+                let filename = path.basename(fileordirpath);
+                let destpath = path.join(dirpath , "others" ,filename);
+                fs.copyFileSync(fileordirpath , destpath);
+            }
+        }
+    }
 }
 module.exports = {
     o:o
--- main.js
+++ main.js
@@ -7,8 +7,7 @@ let input  = process.argv.slice(2);
 let command = input[0];
 if(command == "help"){
     help.helpfn();
-}else if(command = "tree"){
-    tree.p(input[1]);
-}else if(command == "organize"){
+}
+else if(command == "organize"){
     organize.o(input[1]);
 }
